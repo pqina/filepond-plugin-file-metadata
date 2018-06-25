4af387033e3926e42b0b299172b313d7dc8aa452
Rik Schennink <rikschennink@gmail.com>
2018-06-25 15:59:56 +0200

    initial commit

---
+++ dist/filepond-plugin-file-metadata.esm.js
@@ -0,0 +1,58 @@
+/*
+ * FilePondPluginFileMetadata 1.0.1
+ * Licensed under MIT, https://opensource.org/licenses/MIT
+ * Please visit https://pqina.nl/filepond for details.
+ */
+var plugin$1 = ({ addFilter, utils }) => {
+  // get quick reference to Type utils
+  const { Type } = utils;
+
+  // setup attribute mapping for accept
+  addFilter('SET_ATTRIBUTE_TO_OPTION_MAP', map =>
+    Object.assign(map, {
+      '^fileMetadata': {
+        group: 'fileMetadataObject'
+      }
+    })
+  );
+
+  addFilter(
+    'DID_LOAD_ITEM',
+    (item, { query }) =>
+      new Promise((resolve, reject) => {
+        if (!query('GET_ALLOW_FILE_METADATA')) {
+          return resolve(item);
+        }
+
+        // get default object and add as metadata
+        const data = query('GET_FILE_METADATA_OBJECT');
+        if (typeof data === 'object') {
+          Object.keys(data).forEach(key => {
+            item.setMetadata(key, data[key]);
+          });
+        }
+
+        // done!
+        resolve(item);
+      })
+  );
+
+  return {
+    options: {
+      // Enable or disable file renaming
+      allowFileMetadata: [true, Type.BOOLEAN],
+
+      // A metadata object to add to all files
+      fileMetadataObject: [null, Type.OBJECT]
+    }
+  };
+};
+
+if (typeof navigator !== 'undefined' && document) {
+  // plugin has loaded
+  document.dispatchEvent(
+    new CustomEvent('FilePond:pluginloaded', { detail: plugin$1 })
+  );
+}
+
+export default plugin$1;
---
+++ dist/filepond-plugin-file-metadata.js
@@ -0,0 +1,204 @@
+/*
+ * FilePondPluginFileMetadata 1.0.1
+ * Licensed under MIT, https://opensource.org/licenses/MIT
+ * Please visit https://pqina.nl/filepond for details.
+ */
+(function(global, factory) {
+  typeof exports === 'object' && typeof module !== 'undefined'
+    ? (module.exports = factory())
+    : typeof define === 'function' && define.amd
+      ? define(factory)
+      : (global.FilePondPluginFileMetadata = factory());
+})(this, function() {
+  'use strict';
+
+  var _typeof =
+    typeof Symbol === 'function' && typeof Symbol.iterator === 'symbol'
+      ? function(obj) {
+          return typeof obj;
+        }
+      : function(obj) {
+          return obj &&
+            typeof Symbol === 'function' &&
+            obj.constructor === Symbol &&
+            obj !== Symbol.prototype
+            ? 'symbol'
+            : typeof obj;
+        };
+
+  var asyncGenerator = (function() {
+    function AwaitValue(value) {
+      this.value = value;
+    }
+
+    function AsyncGenerator(gen) {
+      var front, back;
+
+      function send(key, arg) {
+        return new Promise(function(resolve, reject) {
+          var request = {
+            key: key,
+            arg: arg,
+            resolve: resolve,
+            reject: reject,
+            next: null
+          };
+
+          if (back) {
+            back = back.next = request;
+          } else {
+            front = back = request;
+            resume(key, arg);
+          }
+        });
+      }
+
+      function resume(key, arg) {
+        try {
+          var result = gen[key](arg);
+          var value = result.value;
+
+          if (value instanceof AwaitValue) {
+            Promise.resolve(value.value).then(
+              function(arg) {
+                resume('next', arg);
+              },
+              function(arg) {
+                resume('throw', arg);
+              }
+            );
+          } else {
+            settle(result.done ? 'return' : 'normal', result.value);
+          }
+        } catch (err) {
+          settle('throw', err);
+        }
+      }
+
+      function settle(type, value) {
+        switch (type) {
+          case 'return':
+            front.resolve({
+              value: value,
+              done: true
+            });
+            break;
+
+          case 'throw':
+            front.reject(value);
+            break;
+
+          default:
+            front.resolve({
+              value: value,
+              done: false
+            });
+            break;
+        }
+
+        front = front.next;
+
+        if (front) {
+          resume(front.key, front.arg);
+        } else {
+          back = null;
+        }
+      }
+
+      this._invoke = send;
+
+      if (typeof gen.return !== 'function') {
+        this.return = undefined;
+      }
+    }
+
+    if (typeof Symbol === 'function' && Symbol.asyncIterator) {
+      AsyncGenerator.prototype[Symbol.asyncIterator] = function() {
+        return this;
+      };
+    }
+
+    AsyncGenerator.prototype.next = function(arg) {
+      return this._invoke('next', arg);
+    };
+
+    AsyncGenerator.prototype.throw = function(arg) {
+      return this._invoke('throw', arg);
+    };
+
+    AsyncGenerator.prototype.return = function(arg) {
+      return this._invoke('return', arg);
+    };
+
+    return {
+      wrap: function(fn) {
+        return function() {
+          return new AsyncGenerator(fn.apply(this, arguments));
+        };
+      },
+      await: function(value) {
+        return new AwaitValue(value);
+      }
+    };
+  })();
+
+  var plugin$1 = function(_ref) {
+    var addFilter = _ref.addFilter,
+      utils = _ref.utils;
+
+    // get quick reference to Type utils
+    var Type = utils.Type;
+
+    // setup attribute mapping for accept
+
+    addFilter('SET_ATTRIBUTE_TO_OPTION_MAP', function(map) {
+      return Object.assign(map, {
+        '^fileMetadata': {
+          group: 'fileMetadataObject'
+        }
+      });
+    });
+
+    addFilter('DID_LOAD_ITEM', function(item, _ref2) {
+      var query = _ref2.query;
+      return new Promise(function(resolve, reject) {
+        if (!query('GET_ALLOW_FILE_METADATA')) {
+          return resolve(item);
+        }
+
+        // get default object and add as metadata
+        var data = query('GET_FILE_METADATA_OBJECT');
+        if (
+          (typeof data === 'undefined' ? 'undefined' : _typeof(data)) ===
+          'object'
+        ) {
+          Object.keys(data).forEach(function(key) {
+            item.setMetadata(key, data[key]);
+          });
+        }
+
+        // done!
+        resolve(item);
+      });
+    });
+
+    return {
+      options: {
+        // Enable or disable file renaming
+        allowFileMetadata: [true, Type.BOOLEAN],
+
+        // A metadata object to add to all files
+        fileMetadataObject: [null, Type.OBJECT]
+      }
+    };
+  };
+
+  if (typeof navigator !== 'undefined' && document) {
+    // plugin has loaded
+    document.dispatchEvent(
+      new CustomEvent('FilePond:pluginloaded', { detail: plugin$1 })
+    );
+  }
+
+  return plugin$1;
+});
