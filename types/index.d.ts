b77cf40dc39c0005e4579dac85d70d86023e1552
Rik Schennink <rikschennink@gmail.com>
2021-03-19 16:35:54 +0100

    fix types

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,11 +1,12 @@
-declare module "filepond-plugin-file-metadata" {
-    const FilepondPluginFileMetadata: FilepondPluginFilepondPluginFileMetadataProps;
-    export interface FilepondPluginFilepondPluginFileMetadataProps {
-        /** Enable or disable the filemetadata object */
-        allowFileMetadata: boolean;
+// @ts-ignore
+import { FilePondOptions } from "filepond";
 
-        /** The object that is used to set the initial metadata object of each file item */
-        fileMetadataObject: undefined | object | null;
-    }
-    export default FilepondPluginFileMetadata;
+declare module "filepond" {
+  export interface FilePondOptions {
+    /** Enable or disable the filemetadata object */
+    allowFileMetadata?: boolean;
+
+    /** The object that is used to set the initial metadata object of each file item */
+    fileMetadataObject?: undefined | object | null;
+  }
 }
